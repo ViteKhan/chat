37ca2be7cc08dec9fda19d6186ac4c293696b177
ViteKhan <42898182+ViteKhan@users.noreply.github.com>
2023-01-01 23:11:45 +0500

    added navbar component

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import { Search } from '../search';
+
+import './navbar-style.scss';
+
+export const Navbar: FC = () => (
+  <div className="Navbar">
+    <div>Olivia Chat</div>
+    <Search/>
+  </div>
+);
