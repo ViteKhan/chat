155e4bc04cf9625287ab614d0350d47b31613275
ViteKhan <42898182+ViteKhan@users.noreply.github.com>
2023-01-01 23:12:03 +0500

    added sidebar component

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import { Chats } from '../chats';
+import { Navbar } from '../navbar';
+
+import './sidebar-style.scss';
+export const Sidebar: FC = () => (
+  <div className="Sidebar">
+    <Navbar/>
+    <Chats/>
+  </div>
+);
